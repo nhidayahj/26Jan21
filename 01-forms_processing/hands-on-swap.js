b6d87347f3ca85ca5a0779de6956f6fc8ab4d6ed
nhidayahj <hidayah.joha@gmail.com>
2021-01-26 05:39:42 +0000

    hands-on practice

--- 01-forms_processing/hands-on-swap.js
+++ 01-forms_processing/hands-on-swap.js
@@ -1,6 +1,13 @@
 let swapBtn = document.querySelector('#swap-btn');
 
 swapBtn.addEventListener('click', function(){
+    //put the left value in a temp storage
+    let temp = document.querySelector("#left").value;
+    //replace the left value from the right value
+    document.querySelector("#left")
+    // replace the right value from the left value
+
+    
     let leftText = document.querySelector('#left').value
     let rightText = document.querySelector('#right').value
     let holdValue;
